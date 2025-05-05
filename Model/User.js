38e2c7a57e1cc22e32bff70c2a95a947c6bd152e
Yug Patel <yugpatelvipulbhai@gmail.com>
2025-05-05 23:28:45 +0530

    id

--- Model/User.js
+++ Model/User.js
@@ -4,7 +4,6 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, trim: true },
   email: { type: String, required: true, unique: true, lowercase: true, trim: true },
   password: { type: String, required: true },
-  SocketID: { type: String, required: true, unique: true },
   isFollowing: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User', default: [] }],
   isFollower: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User', default: [] }],
   isOnline:{type:Boolean,default:false}
